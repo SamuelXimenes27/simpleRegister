855654c777166b413c5bd7dc7c0f678ee59fa8f7
SamuelXimenes <samuel.ximenes.oliveira60@aluno.ifce.edu.br>
2022-01-04 19:14:40 -0300

    lógicas dos botões de editar, excluir e adicionar finalizados

--- lib/components/user_tile.dart
+++ lib/components/user_tile.dart
@@ -2,7 +2,9 @@
 /* ARQUIVO NO DIRETORIO 'lib/components/user_tile.dart'*/
 
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:register/models/user.dart';
+import 'package:register/provider/users.dart';
 import 'package:register/routes/app_routes.dart';
 
 class UserTile extends StatelessWidget {
@@ -24,6 +26,7 @@ class UserTile extends StatelessWidget {
           children: <Widget>[
             IconButton(
               icon: Icon(Icons.edit),
+              color: Colors.yellow,
               onPressed: () {
                 Navigator.of(context).pushNamed(
                   AppRoutes.USER_FORM,
@@ -32,10 +35,31 @@ class UserTile extends StatelessWidget {
               },
             ), //IconButton
             IconButton(
-              icon: Icon(Icons.delete),
-              color: Colors.red,
-              onPressed: () {},
-            ), //IconButton
+                icon: Icon(Icons.delete),
+                color: Colors.red,
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (ctx) => AlertDialog(
+                      title: Text('Excluir Usuário'),
+                      content: Text('Tem certeza?'),
+                      actions: <Widget>[
+                        FlatButton(
+                          onPressed: () => Navigator.of(context).pop(true),
+                          child: Text('Sim'),
+                        ),
+                        FlatButton(
+                          onPressed: () => Navigator.of(context).pop(false),
+                          child: Text('Não'),
+                        ),
+                      ],
+                    ),
+                  ).then((confirmed) {
+                    if (confirmed) {
+                      Provider.of<Users>(context, listen: false).remove(user);
+                    }
+                  });
+                }) //IconButton
           ], //<Widget>
         ), //Row
       ), //Container
--- lib/views/user_form.dart
+++ lib/views/user_form.dart
@@ -1,13 +1,99 @@
 import 'package:flutter/widgets.dart';
 
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:register/models/user.dart';
+import 'package:register/provider/users.dart';
 
-class UserForm extends StatelessWidget {
+class UserForm extends StatefulWidget {
+  @override
+  State<UserForm> createState() => _UserFormState();
+}
+
+class _UserFormState extends State<UserForm> {
+  final _form = GlobalKey<FormState>();
+
+  final Map<String?, String?> _formData = {};
+
+  void _loadFormData(User user) {
+    if (user != null) {
+      _formData['id'] = user.id;
+      _formData['name'] = user.name;
+      _formData['email'] = user.email;
+      _formData['avatarURL'] = user.avatarURL;
+    }
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    final user = ModalRoute.of(context)!.settings.arguments as User;
+    _loadFormData(user);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Formulário de Usuário'),
+        actions: <Widget>[
+          IconButton(
+            onPressed: () {
+              final isValid = _form.currentState!.validate();
+
+              if (isValid) {
+                _form.currentState!.save();
+
+                Provider.of<Users>(context, listen: false).put(
+                  User(
+                    id: _formData['id'] ?? ' ',
+                    name: _formData['name'] ?? ' ',
+                    email: _formData['email'] ?? ' ',
+                    avatarURL: _formData['avatarURL'] ?? ' ',
+                  ),
+                );
+
+                Navigator.of(context).pop();
+              }
+            },
+            icon: Icon(Icons.save),
+          )
+        ],
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(10),
+        child: Form(
+          key: _form,
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                initialValue: _formData['name'],
+                decoration: InputDecoration(labelText: 'Nome'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Ocorreu um erro";
+                  }
+                  if (value.trim().length < 5) {
+                    return "Nome muito pequeno. No minimo 5 letras.";
+                  }
+                  return null;
+                },
+                onSaved: (value) => _formData['name'] = value,
+              ),
+              TextFormField(
+                initialValue: _formData['email'],
+                decoration: InputDecoration(labelText: 'Email'),
+                onSaved: (value) => _formData['email'] = value,
+              ),
+              TextFormField(
+                initialValue: _formData['avatarURL'],
+                decoration: InputDecoration(labelText: 'URL do Avatar'),
+                onSaved: (value) => _formData['avatarURL'] = value,
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
